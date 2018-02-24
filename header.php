ab477c8f16067d123559a750818f6e6e4226b973
GitCoderr <allonsytoutdesuite@gmail.com>
2018-02-25 03:42:35 +0800

    Added color coding to levels and categories

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1" >
 		<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
