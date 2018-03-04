f44b3e64a222867594c8a2df3bc63918332e4631
GitCoderr <allonsytoutdesuite@gmail.com>
2018-03-04 17:34:06 +0800

    Remove duplicates from quizzes taken list

--- listquizzestaken.php
+++ listquizzestaken.php
@@ -1,10 +1,19 @@
-<?php if (isset($_SESSION['username']): ?>
-	$username = $_SESSION['username'];
-	echo "<p>You have done the following quizzes:</p>";
-	include "dbh.php";
-	$sql = "SELECT * FROM quizzes_taken WHERE username = '".$username."'";
-	$result = mysqli_query($conn, $sql);
-	while ($row = mysqli_fetch_array($result)) {
-		echo "<p> Quiz #".$row['quizId']."</p>";
-	}
+<?php	if (isset($_SESSION['username'])): ?>
+	<p>You have done the following quizzes:</p>
+	<?php
+		include "dbh.php";
+		$username = $_SESSION['username'];
+		$sql = "SELECT * FROM quizzes_taken WHERE username = '".$username."'";
+		$result = mysqli_query($conn, $sql);
+		$quizzesTakenList = array();
+	?>
+	<?php
+		while ($row = mysqli_fetch_array($result)) {
+			array_push($quizzesTakenList, $row['quizId']);
+		}
+		$quizzesTakenList = array_unique($quizzesTakenList);
+	?>
+	<?php	foreach($quizzesTakenList as $quizTaken): ?>
+			<p>Quiz #<?php echo $quizTaken; ?></p>
+	<?php endforeach; ?>
 <?php endif; ?>
