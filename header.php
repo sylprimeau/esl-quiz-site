ae6f8a39aeb42373dde918b23d9c3856d50ccab7
GitCoderr <allonsytoutdesuite@gmail.com>
2018-03-11 14:15:45 +0800

    Changed styles a bit on profile page

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <html lang="en">
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1" >
-		<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700" rel="stylesheet">
 		<link rel="stylesheet" href="style.css" type="text/css">
 		<title>The ESL Quiz Site</title>
 	</head>
