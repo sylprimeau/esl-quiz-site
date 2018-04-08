b9a8d29ad9486a74ef9442376466075922829d7e
GitCoderr <allonsytoutdesuite@gmail.com>
2018-04-08 13:43:37 +0800

    Added FB share button and improved styling of login page

--- header.php
+++ header.php
@@ -20,8 +20,19 @@
 	</head>
 	
 	<body>
+<!--		FB share button SDK code-->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=468601610243777&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+		
 			<header>
 				<nav>
+<div class="fb-share-button" data-href="https://www.eslquizsite.com" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.eslquizsite.com%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"></a></div>
 					<div class="hamburger-menu hide">
 						<div class="lines"></div>
 						<div class="lines"></div>
