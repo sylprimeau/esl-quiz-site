7f41fefac5c0b1af60f1416635c38625fb8f4836
sylprimeau <allonsytoutdesuite@gmail.com>
2021-06-23 23:28:14 +0700

    Fixed so that 'delete account' button doesn't show on non-logged in profile page

--- profile.php
+++ profile.php
@@ -11,18 +11,20 @@
 		</form>
 		
 		<?php include "listquizzestaken.php"; ?>
+		<p class="warning-text">Warning! Clicking the "Delete account" button will delete your account and remove ALL information tied to that account. There is no way to retrieve it afterwards!</p>
+		<button class="delete-account">Delete account</button>
 		
 	<?php endif; ?>
-	<p class="warning-text">Warning! Clicking the "Delete account" button will delete your account and remove ALL information tied to that account. There is no way to retrieve it afterwards!</p>
-	<button class="delete-account">Delete account</button>
 </section>
 
 <script>
 	// delete account
 	var deleteAccountBtn = document.querySelector(".delete-account");
-	deleteAccountBtn.addEventListener("click", function() {
-		alert("Sorry. This functionality is still in the development phase. Your account cannot be deleted at this time.");
-	});
+	deleteAccountBtn.addEventListener("click", deleteUserAcct);
+
+	function deleteUserAcct() {
+		alert("Sorry! This functionality is still in the development phase. Your account cannot be deleted at this time.");
+	}
 </script>
 
 
