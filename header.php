c5796746875b0342612f5039f0c42d0561ff561b
sylprimeau <allonsytoutdesuite@gmail.com>
2021-06-24 19:18:29 +0700

    Facebook share button stopped working for some reason so had to swap out the FB code

--- header.php
+++ header.php
@@ -22,21 +22,14 @@
 	
 	<body>
 <!--		FB share button SDK code-->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=468601610243777&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-		
+	<div id="fb-root"></div>
+	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v11.0&appId=468601610243777&autoLogAppEvents=1" nonce="3EVAppYm"></script>
 			<header>
 					<?php if (!isset($_SESSION['username'])): ?>
 						<div class="login-prompt">Login to keep track of your history!</div>
 					<?php endif; ?>
 				<nav>
-<div class="fb-share-button" data-href="https://www.eslquizsite.com" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.eslquizsite.com%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"></a></div>
+				<div class="fb-share-button" data-href="https://www.eslquizsite.com/" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.eslquizsite.com%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
 					<div class="hamburger-menu hide">
 						<div class="lines"></div>
 						<div class="lines"></div>
