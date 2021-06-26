fd4d42542f406747f73cd14317a22c3279cb9881
sylprimeau <allonsytoutdesuite@gmail.com>
2021-06-27 05:20:27 +0700

    Fixed: Keeps user on login page when entering wrong login info instead of redirecting to homepage

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -12,17 +12,19 @@ $hash_pwd = $row['password']; // gets hashed pwd from db
 $pwdMatch = password_verify($password, $hash_pwd); // checks match for typed pwd vs hash in db, returns TRUE if they match
 
 if ($pwdMatch == 0) {
-	header("Location: ../index.php?error=empty");
+	header("Location: ../login.php?error=empty");
 } else {
-	$sql = "SELECT * FROM users WHERE username='$username' AND password='$hash_pwd'";
-	$result = mysqli_query($conn, $sql);
+	// This part here doesn't make sense. By the time you get to this part, we've already found a user and password and they matched!
+	// Seems to work with code commented out but keep it for a while just in case. If no problems then delete. Comment date: June 27, 2021
+	// $sql = "SELECT * FROM users WHERE username='$username' AND password='$hash_pwd'";
+	// $result = mysqli_query($conn, $sql);
 
-	if (!$row = mysqli_fetch_assoc($result)) {
-		echo "Your username or password is incorrect.";
-	} else {
+	// if (!$row = mysqli_fetch_assoc($result)) {
+	// 	echo "Your username or password is incorrect.";
+	// } else {
 		$_SESSION['id'] = $row['id'];
 		$_SESSION['username'] = $row['username'];
-	}
+	// }
 
 	header("Location: ../index.php");
 }
