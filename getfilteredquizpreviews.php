d59aaedc3c870c400f5af92cf76e1db2c7effacf
sylprimeau <allonsytoutdesuite@gmail.com>
2021-07-01 17:28:28 +0700

    Fixed alignment and positioning of titles and descriptions on quiz previews.

--- getfilteredquizpreviews.php
+++ getfilteredquizpreviews.php
@@ -29,28 +29,32 @@
 ?>
 
 <div class="quiz-preview quiz-random-start">
-	<h3 class="title">Random Quiz</h3>
-	<p class="description">Click here to do a random quiz or select one from the list below. Use the filters to restrict quiz selection by level and categories!</p>
+	<div class="quiz-preview-text">
+		<h3 class="title">Random Quiz</h3>
+		<p class="description">Click here to do a random quiz or select one from the list below. Use the filters to restrict quiz selection by level and categories!</p>
+	</div>
 </div>
 
 <?php while ($row = mysqli_fetch_array($result)): ?>
 	<div class="quiz-preview quiz-specific-start" data-quizid=<?php echo $row['quizId']; ?>>
-		<?php if (isset($_SESSION['username'])): ?>
-			<?php if (in_array($row['quizId'], $quizzesCompletedList)): ?>
-				<h5 class="completed">DONE!</h5>
-			<?php elseif (in_array($row['quizId'], $quizzesStartedList)): ?>
-				<h5 class="started">INCOMPLETE!</h5>
-			<?php endif; ?>
-		<?php endif; ?>
-		<h3 class="title"><?php echo $row['title']; ?></h3>
 		<?php if (isset($row['quizImgUrl'])): ?>
 			<?php if ($row['quizImgUrl'] <> ''): ?>
 				<img class="quizImgUrl" src="<?php echo $row['quizImgUrl']; ?>" alt="">
 			<?php endif; ?>
 		<?php endif; ?>
-		<p class="description"><?php echo $row['description']; ?></p>
-		<h5 class="category <?php echo $row['category']; ?>"><?php echo $row['category']; ?></h5>
-		<h5 class="level level<?php echo $row['level']; ?>">Level <?php echo $row['level']; ?></h5>
+		<div class="quiz-preview-text">
+			<h3 class="title"><?php echo $row['title']; ?></h3>
+			<p class="description"><?php echo $row['description']; ?></p>
+			<h5 class="category <?php echo $row['category']; ?>"><?php echo $row['category']; ?></h5>
+			<h5 class="level level<?php echo $row['level']; ?>">Level <?php echo $row['level']; ?></h5>
+			<?php if (isset($_SESSION['username'])): ?>
+				<?php if (in_array($row['quizId'], $quizzesCompletedList)): ?>
+					<h5 class="completed">DONE!</h5>
+				<?php elseif (in_array($row['quizId'], $quizzesStartedList)): ?>
+					<h5 class="started">INCOMPLETE!</h5>
+				<?php endif; ?>
+			<?php endif; ?>
+		</div>
 	</div>
 <?php endwhile; ?>
 
